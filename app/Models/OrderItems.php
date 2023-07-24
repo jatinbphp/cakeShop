37b4af1b71abb08711b34465f6828fb0fdfc564b
Jatin Bhatt <jatin.b.php@gmail.com>
2023-07-24 16:12:06 +0530

    order section

--- app/Models/OrderItems.php
+++ app/Models/OrderItems.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OrderItems extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable= ['order_id','product_id','name','sku','quantity','price','total'];
 }
