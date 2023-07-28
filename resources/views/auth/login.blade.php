6407503c8f230537e957271aed5ba59e78a7fea6
Jatin Bhatt <jatin.b.php@gmail.com>
2023-07-28 16:21:25 +0530

    facebook login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,6 +65,12 @@
                                                 {{ __('Forgot Your Password?') }}
                                             </a>
                                         @endif
+
+                                        <div class="flex items-center justify-end mt-4">
+                                            <a class="ml-1 btn btn-link" href="{{ url('auth/facebook') }}" style="margin-top: 0px !important;background: blue;color: #ffffff;padding: 5px;border-radius:7px;" id="btn-fblogin">
+                                                <i class="fa fa-facebook-square" aria-hidden="true"></i> Login with Facebook
+                                            </a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -87,34 +87,36 @@
         </div>
     </div>
 </section>
+
 <section class="popular-items section-padding40">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-xl-6 col-lg-8 col-md-10 col-sm-10">
+            <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
                 <div class="section-tittle mb-60">
                     <h2>When do you want your order?</h2>
                     <p>Showing next available dates <a style="color:#000;float: right;" href="javascript:void(0)">More dates</a></p>
                 </div>
             </div>
         </div>
-        <div class="popular-active">
-            <div class="row">
-                <div class="col-xl-12 col-lg-8 col-md-10 col-sm-10">
+        <div class="orderProcess">
+            <div class="row justify-content-center">
+                <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
                     <div class="rescalendar" id="my_calendar_calSize"></div>
                 </div>
-                <div class="col-xl-12 col-lg-8 col-md-10 col-sm-10">
-                    <p>Times shown in Asia/Manila time</p>
-                    
-                    <select class="order_time" name="order_time">
-                        <option value="09:00">09:00 AM</option>
-                        <option value="10:00">10:00 AM</option>
-                        <option value="13:00">01:00 PM</option>
-                        <option value="14:00">02:00 PM</option>
-                        <option value="15:00">03:00 PM</option>
-                        <option value="16:00">04:00 PM</option>
-                        <option value="17:00">05:00 PM</option>
-                        <option value="18:00">06:00 PM</option>
-                    </select>
+                <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
+                    <div class="form-group">
+                        <label>Times shown in Asia/Manila time</label>
+                        <select class="order_time" name="order_time">
+                            <option value="09:00">09:00 AM</option>
+                            <option value="10:00">10:00 AM</option>
+                            <option value="13:00">01:00 PM</option>
+                            <option value="14:00">02:00 PM</option>
+                            <option value="15:00">03:00 PM</option>
+                            <option value="16:00">04:00 PM</option>
+                            <option value="17:00">05:00 PM</option>
+                            <option value="18:00">06:00 PM</option>
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
@@ -123,17 +125,19 @@
 <section class="popular-items section-padding40">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-xl-6 col-lg-8 col-md-10 col-sm-10">
+            <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
                 <div class="section-tittle mb-60">
                     <h2>What's your name?</h2>
                 </div>
             </div>
         </div>
-        <div class="popular-active">
-            <div class="row">
-                <div class="col-xl-12 col-lg-8 col-md-10 col-sm-10">
-                    <input type="text" name="" placeholder="Enter Here">
-                    <button>Next</button>
+        <div class="orderProcess">
+            <div class="row justify-content-center">
+                <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
+                    <div class="form-group">
+                        <input type="text" name="" placeholder="Enter Here" class="form-control">
+                    </div>
+                    <button class="btn btn-primary">Next</button>
                 </div>
             </div>
         </div>
@@ -142,20 +146,24 @@
 <section class="popular-items section-padding40">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-xl-6 col-lg-8 col-md-10 col-sm-10">
+            <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
                 <div class="section-tittle mb-60">
                     <h2>What's your email?</h2>
                     <p>Your order confirmation will be sent here</p>
                 </div>
             </div>
         </div>
-        <div class="popular-active">
-            <div class="row">
-                <div class="col-xl-12 col-lg-8 col-md-10 col-sm-10">
-                    <input type="text" name="" placeholder="coco@gmail.com">
-                    <p><input type="checkbox" name="">I'd like to receive shop offers and updates through email
-                    </p></br>
-                    <button>Next</button>
+        <div class="orderProcess">
+            <div class="row justify-content-center">
+                <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
+                    <div class="form-group">
+                        <input type="text" name="" placeholder="coco@gmail.com" class="form-control">
+                    </div>
+                    <div class="form-group d-flex align-items-center">
+                        <input class="form-check-input" type="checkbox" name="">
+                        <label class="m-0">I'd like to receive shop offers and updates through email</label>
+                    </div>
+                    <button class="btn btn-primary">Next</button>
                 </div>
             </div>
         </div>
@@ -164,18 +172,20 @@
 <section class="popular-items section-padding40">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-xl-6 col-lg-8 col-md-10 col-sm-10">
+            <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
                 <div class="section-tittle mb-60">
                     <h2>What's your phone number?</h2>
                     <p>No spam. For order purposes only</p>
                 </div>
             </div>
         </div>
-        <div class="popular-active">
-            <div class="row">
-                <div class="col-xl-12 col-lg-8 col-md-10 col-sm-10">
-                    <input type="text" name="" placeholder="081234 56789">
-                    <button>Next</button>
+        <div class="orderProcess">
+            <div class="row justify-content-center">
+                <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
+                    <div class="form-group">
+                        <input type="text" name="" placeholder="081234 56789" class="form-control">
+                    </div>
+                    <button class="btn btn-primary">Next</button>
                 </div>
             </div>
         </div>
@@ -184,17 +194,19 @@
 <section class="popular-items section-padding40">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-xl-6 col-lg-8 col-md-10 col-sm-10">
+            <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
                 <div class="section-tittle mb-60">
                     <h2>Do you want to write a short note? Type below.</h2>
                 </div>
             </div>
         </div>
-        <div class="popular-active">
-            <div class="row">
-                <div class="col-xl-12 col-lg-8 col-md-10 col-sm-10">
-                    <input type="text" name="" placeholder="Enter Here">
-                    <button>Next</button>
+        <div class="orderProcess">
+            <div class="row justify-content-center">
+                <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
+                    <div class="form-group">
+                        <input type="text" name="" placeholder="Enter Here" class="form-control">
+                    </div>
+                    <button class="btn btn-primary">Next</button>
                 </div>
             </div>
         </div>
@@ -204,22 +216,29 @@
 <section class="popular-items section-padding40">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-xl-6 col-lg-8 col-md-10 col-sm-10">
+            <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
                 <div class="section-tittle mb-60">
                     <h2>And finally, how would you like to pay?</h2>
                 </div>
             </div>
         </div>
-        <div class="popular-active">
-            <div class="row">
-                <div class="col-xl-12 col-lg-8 col-md-10 col-sm-10">
-                    <input type="radio" id="html" name="fav_language" value="HTML">
-                    <label for="html">Credit/Debit Card</label><br>
-                    <input type="radio" id="css" name="fav_language" value="CSS">
-                    <label for="css">GCash</label><br>
-                    <input type="radio" id="javascript" name="fav_language" value="JavaScript">
-                    <label for="javascript">Bank Transfer (Security Bank)</label>
-
+        <div class="orderProcess">
+            <div class="row justify-content-center">
+                <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10">
+                    <div class="radio-group-list">
+                        <div class="radio-group-item">
+                            <input type="radio" id="html" name="fav_language" value="HTML">
+                            <label for="html">Credit/Debit Card</label>
+                        </div>
+                        <div class="radio-group-item">
+                            <input type="radio" id="css" name="fav_language" value="CSS">
+                            <label for="css">GCash</label>
+                        </div>
+                        <div class="radio-group-item">
+                            <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+                            <label for="javascript">Bank Transfer (Security Bank)</label>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
